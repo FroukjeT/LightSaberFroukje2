3a0c6ea634b843139cbfeb1f9c7b7897e9aa65b8
Cloudperry <roni.hokkanen@protonmail.com>
2025-09-15 20:42:04 +0300

    end of monday

--- Assets/Swords.cs
+++ Assets/Swords.cs
@@ -6,6 +6,8 @@ public class Swords : MonoBehaviour
     private Vector3 lastpos;
     private Vector3 velocity;
 
+    public string swordColorTag = "Red";
+
     //here we measure the velocity every frame 
     void Update()
     {
@@ -19,12 +21,15 @@ public class Swords : MonoBehaviour
         MovingCube cube = other.GetComponent<MovingCube>(); //so this gets the reference to the movingCube script, if it doesnt exist it exits
         if (cube == null) return;
 
+        //check color tag
+        if(other.tag != swordColorTag) return; //if the tag of the collided object is not the same as the sword, exit (do nothing
+
         Vector3 swingDir = velocity.normalized; //normalize it
 
         // Compare with required direction
         float dot = Vector3.Dot(swingDir, cube.requiredDirection); //so you compare its sword (SwingDir) with the cube (req direct)
 
-        if (dot > 0.7f) // 1 = perfect alignment, 0 = perpendicular
+        if (dot > 0.6f) // 1 = perfect alignment, 0 = perpendicular
         {
             Debug.Log("Correct slice!");
             Destroy(cube.gameObject);
