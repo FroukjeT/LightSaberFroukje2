be44e1819503beff9e263e2ddfb2926d371553d3
Cloudperry <roni.hokkanen@protonmail.com>
2025-09-15 17:49:47 +0300

    sdf

---
+++ Assets/CubeSpawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [Header("Prefabs")]
+    public GameObject[] cubePrefabs; //assign 4 prefabs here
+
+    [Header("Spawn points")]
+    public Transform[] spawnPoints; //assign 4 prefabs here
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
